711645d8d06fb5845544e7c2918cbf54e9dfa8bc
MarcosGNobre <joaoeduardo200916@gmail.com>
2026-05-25 16:59:30 -0300

    projeto_cdt

--- projeto _cdt.py
+++ projeto _cdt.py
@@ -7,11 +7,43 @@ usuario = input("Usuário: ")
 senha = input("Senha: ")
 
 if usuario == usuario_correto and senha == senha_correta:
+
     print("acesso permitido")
     print("bem vindo", usuario)
+
+    print("sistema liberado")
+    print("carregando...")
+    print("\n=== SISTEMA DO SISTEMA ===")
+    print("1 - Ver status ")
+    print("2 - Abrir  painel")
+    print("3 - Encerrar")
+
+    opcao = input(" Ecolha uma opção")
+
+if opcao == "1":
+    
+    print("Sistema protegido")
+    print("Nenhuma ameaça detectada")
+
+elif opcao == "2":
+
+    print("PAinel aberto")
+    print("Carregando informações....")
+
+elif == "3":
+
+    print("Sistema encerrado")
+    print("Voltando ao menu")
+
+
+
 
 else:
     print("acesso negado")
     print("opçao invalida")
     print("tente novamente")
+
+    print("usuario bloqueado")
+    print("voltando ao menu")
+
     
